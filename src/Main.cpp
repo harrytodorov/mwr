052290a096ebb8b1ffd70fb731e2b0e5ddb5c966
Haralambi Todorov <harrytodorov@gmail.com>
2019-02-08 00:13:21 +0100

    InAWeekend, chapter 1: * output a gradient PPM image

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,41 @@
 // Copyright (c) 2019, University of Freiburg.
 // Author: Haralambi Todorov <harrytodorov@gmail.com>
 
+#include <iostream>
+#include <fstream>  // Output to a file
+
+#include "Vec3.h"
+
 int main() {
-  return 0;
+  int nx = 200;
+  int ny = 100;
+
+  std::ofstream image_file;
+  image_file.open("rendered_image.ppm");
+
+  // PPM header
+  image_file << "P3" << std::endl
+             << nx << " " << ny << std::endl
+             << "255" << std::endl;
+
+  // The actual image pixel
+  for (int j = ny - 1; j >= 0; j--) {
+    for (int i = 0; i < nx; i++) {
+      // RGB pattern
+      float r = float(i) / float(nx);
+      float g = float(j) / float(ny);
+      float b = 0.2;
+
+      // Convert floats to ints
+      int ir = int(255.99f * r);
+      int ig = int(255.99f * g);
+      int ib = int(255.99f * b);
+
+      // Print pixel
+      image_file << ir << " " << ig << " " << ib << std::endl;
+    }
+  }
+
+  // Close file
+  image_file.close();
 }
