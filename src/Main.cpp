0f2a914ade18fa9167404a0f49769fbc3138f88e
Haralambi Todorov <harrytodorov@gmail.com>
2019-03-17 15:37:09 +0100

    InAWeekend, chapter 7: Diffuse materials * generate a random point inside the unit sphere * simulate diffuse material * add gamma correction

--- src/Main.cpp
+++ src/Main.cpp
@@ -13,9 +13,41 @@
 #include "HitableList.h"
 #include "Camera.h"
 
+
+// Define random number generation globally
+std::random_device seed; // Random number seed
+std::mt19937 eng(seed()); // Random number engine
+std::uniform_real_distribution<float> dist_01(0.f, 1.f);
+std::uniform_real_distribution<float> dist_11(-1.f, 1.f);
+
+// Define shadow bias
+#define SHADOW_BIAS 0.001f
+
+/**
+ * Pick a random point in the unit sphere using the rejection method.
+ * Algoritmh:
+ * 1. Generate a random point inside the unit cube just with x,y,z having
+ *    random values between [-1, 1).
+ * 2. Generate new points while the squred length of the point is larger than 1
+ */
+Vec3 random_in_unit_sphere() {
+  Vec3 point;
+  do {
+    point[0] = dist_11(eng);
+    point[1] = dist_11(eng);
+    point[2] = dist_11(eng);
+  } while (point.squared_length() >= 1.f);
+  return point;
+}
+
 Vec3 color(const Ray &r, Hitable *world) {
   HitableRecord rec;
-  if (world->hit(r, 0.f, MAXFLOAT, rec)) {
+  if (world->hit(r, SHADOW_BIAS, MAXFLOAT, rec)) {
+    Vec3 target_direction = rec.normal + random_in_unit_sphere();
+    // Recursive call
+    return 0.5f * color(Ray(rec.p, target_direction), world);
+    // Same trick as above to make the range of the normal from [-1, 1] to
+    // [0, 2] and then to [0, 1]
     return 0.5f*Vec3(rec.normal.x() + 1.f,
                      rec.normal.y() + 1.f,
                      rec.normal.z() + 1.f);
@@ -40,12 +72,9 @@ int main() {
   int nx = 200;
   int ny = 100;
   int ns = 100; // Number of samples
-  std::random_device seed; // Random number seed
-  std::mt19937 eng(seed()); // Random number engine
-  std::uniform_real_distribution<float> dist(0.f, 1.f);
 
   std::ofstream image_file;
-  image_file.open("rendered_image_aliasing.ppm");
+  image_file.open("rendered_image_aliasing_gamma.ppm");
 
   // PPM header
   image_file << "P3" << std::endl
@@ -70,8 +99,8 @@ int main() {
       // Antialiasing
       for (int s = 0; s < ns; s++) {
         // Get the sample parameters
-        float u = static_cast<float>(i + dist(eng)) / static_cast<float>(nx);
-        float v = static_cast<float>(j + dist(eng)) / static_cast<float>(ny);
+        float u = static_cast<float>(i + dist_01(eng)) / static_cast<float>(nx);
+        float v = static_cast<float>(j + dist_01(eng)) / static_cast<float>(ny);
 
         // Create the ray
         Ray r = cam.get_ray(u, v);
@@ -82,6 +111,11 @@ int main() {
 
       // Box sampling
       col /= static_cast<float>(ns);
+
+      // Gamma correction
+      col[0] = sqrt(col[0]);
+      col[1] = sqrt(col[1]);
+      col[2] = sqrt(col[2]);
 
       // Convert floats to ints
       int ir = static_cast<int>(255.99f * col.r());
