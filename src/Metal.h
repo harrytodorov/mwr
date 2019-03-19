0d2f60f600d5534d01bd826829a48643c6eb233b
Haralambi Todorov <harrytodorov@gmail.com>
2019-03-19 16:05:05 +0100

    * the metal material

---
+++ src/Metal.h
@@ -0,0 +1,40 @@
+// Copyright (c) 2019, University of Freiburg.
+// Author: Haralambi Todorov <harrytodorov@gmail.com>
+
+#ifndef SRC_METAL_H_
+#define SRC_METAL_H_
+
+#include "Material.h"
+#include "Vec3.h"
+#include "Ray.h"
+#include "Hitable.h"
+#include "Utils.h"
+
+
+class Metal: public Material {
+ public:
+  Metal() = default;
+  explicit Metal(const Vec3 &albedo) : _albedo(albedo) {}
+
+  virtual bool scatter(const Ray &r,
+                       const HitRecord &rec,
+                       Vec3 &attenuation,
+                       Ray &scattered) const;
+ private:
+  Vec3 _albedo{0.f, 0.f, 0.f};
+};
+
+// _____________________________________________________________________________
+bool Metal::scatter(const Ray &r,
+                    const HitRecord &rec,
+                    Vec3 &attenuation,
+                    Ray &scattered) const {
+  Vec3 reflected = reflect(make_unit_vector(r.direction()), rec.normal);
+  scattered.origin(rec.p);
+  scattered.direction(reflected);
+  attenuation = _albedo;
+  bool same_direction = (dot(reflected, rec.normal) > 0.f);
+  return same_direction;
+}
+
+#endif  // SRC_METAL_H_
