8ec935e4b68a52f3a8b8169710c17a484db80570
Haralambi Todorov <harrytodorov@gmail.com>
2019-03-19 16:04:19 +0100

    * cpplint: blank line at the end of the file

--- src/Lambertian.h
+++ src/Lambertian.h
@@ -23,9 +23,9 @@ class Lambertian: public Material {
 
 // _____________________________________________________________________________
 bool Lambertian::scatter(const Ray &r,
-                       const HitRecord &rec,
-                       Vec3 &attenuation,
-                       Ray &scattered) const {
+                         const HitRecord &rec,
+                         Vec3 &attenuation,
+                         Ray &scattered) const {
   Vec3 target_direction = rec.normal + random_in_unit_sphere();
   scattered.origin(rec.p);
   scattered.direction(target_direction);
